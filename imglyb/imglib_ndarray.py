5509b122767ecfc10ebc6389876ab96cf358f4d0
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-04-06 15:07:54 -0400

    Update imglib_ndarray.py

--- imglyb/imglib_ndarray.py
+++ imglyb/imglib_ndarray.py
@@ -12,6 +12,7 @@ Arrays = autoclass( 'java.util.Arrays' )
 OwningFloatUnsafe = autoclass( 'net.imglib2.img.basictypelongaccess.unsafe.owning.OwningFloatUnsafe' )
 Fraction = autoclass( 'net.imglib2.util.Fraction' )
 LongStream = autoclass('java.util.stream.LongStream')
+ReshapeView = autoclass( 'net.imglib2.python.view.reshape.ReshapeView' )
 
 
 class Bunch( object ):
@@ -62,7 +63,7 @@ class ImgLibReferenceGuard( np.ndarray ):
 		print( "Contents? ", pointer.contents, UnsafeUtil.UNSAFE.getFloat( address ) )
 		order = 'C'
 
-		obj = np.ndarray.__new__( cls, buffer=np.ctypeslib.as_array( pointer, shape=shape ), shape=shape,dtype=dtype )
+		obj = np.ndarray.__new__( cls, buffer=np.ctypeslib.as_array( pointer, shape=shape ), shape=shape, dtype=dtype )
 		obj.setflags( write = True )
 		obj.rai = rai
 		print( "ADDRESS MISMATCH? ", address, obj.ctypes.data )
@@ -78,30 +79,36 @@ class ImgLibNumpyArray( np.ndarray ):
 
 	def __new__( cls, rai ):
 
-		class_name = util.Helper.className( rai )
+		class_name = util.Helpers.className( rai )
+
+		print( "CLASS NAME {}".format( class_name ) )
 
 		if 'ReferenceGuardingRandomAccessibleInterval' in class_name:
 			return cast( class_name, rai ).getReferenceHolder().args[ 0 ]
 
 		elif 'ArrayImg' in class_name:
-			pass
+			return ImgLibReferenceGuard( rai )
 
 		elif 'UnsafeImg' in class_name:
-			pass
+			return ImgLibReferenceGuard( rai )
 
-		access = rai.randomAccess()
-		rai.min( access )
-		imglib_type = util.Helpers.classNameSimple( access.get() )
-		dtype = dtype_selector[ imglib_type ]
-		shape = tuple( Intervals.dimensionsAsLongArray( rai ) )[::-1]
-		# obj = Bunch( rai=rai, shape=shape, dtype=dtype )
-		obj = np.ndarray.__new__( cls, shape=shape, dtype=dtype, buffer=None, offset=0, strides=None, order=None )
-		setattr( obj, 'rai', rai )
-		setattr( obj, 'imglib_type', imglib_type )
+		else:
+			access = rai.randomAccess()
+			rai.min( access )
+			imglib_type = util.Helpers.classNameSimple( access.get() )
+			dtype = dtype_selector[ imglib_type ]
+			shape = tuple( Intervals.dimensionsAsLongArray( rai ) )[::-1]
+			# obj = Bunch( rai=rai, shape=shape, dtype=dtype )
+			obj = np.ndarray.__new__( cls, shape=shape, dtype=dtype, buffer=None, offset=0, strides=None, order=None )
+			setattr( obj, 'rai', rai )
+			setattr( obj, 'imglib_type', imglib_type )
+			print( "WATTTTZ? {} {}".format(  type(obj), cls ) )
+			print()
+			return obj
 
-		return obj
 
 	def __array_finalize__( self, obj ):
+		print( "ARRAY FINALIZE! {} {} {}".format( obj, type( self ), type( obj ) ) )
 		if obj is None:
 			return
 
@@ -110,36 +117,53 @@ class ImgLibNumpyArray( np.ndarray ):
 			self.shape = obj.shape # getattr( obj, 'shape', None )
 			self.dtype = obj.dtype # getattr( obj, 'dtype', None )
 			self.imglib_type = obj.imglib_type
+			c = util.Views.flatIterable( self.rai ).cursor()
+			while c.hasNext():
+				print( "ARRAY CONTENTS", c.next() )
 
 		else:
 			nd.array.__array_finalize__( self, obj )
 
 	def __getitem__( self, obj ):
-		slicing = [ slice( None ) for d in range( self.rai.numDimensions() ) ] 
-		print( obj, slicing )
+		ret_val = self._getitem_impl( obj )
+		return ret_val
 
-		if isinstance( obj, ( tuple, list ) ):
+	def _getitem_impl( self, obj ):
+		# print( "THIS IS OBJ!", obj, self.shape, type( self ), Intervals.minAsLongArray( self.rai ) )
+		slicing = [ slice( None ) for d in range( self.rai.numDimensions() ) ]
+
+		if obj is Ellipsis:
+			pass
+
+		elif isinstance( obj, ( tuple, list ) ):
 			obj = obj[ :self.rai.numDimensions() ]
 			if np.all( [ isinstance( x, ( int, slice ) ) or x is Ellipsis for x in obj ] ):
 				ellipsis_position = np.where( [ x is Ellipsis for x in obj ] )[ 0 ]
 				ellipsis_position = len( slicing ) if ellipsis_position.size == 0 else ellipsis_position[ 0 ]
-				print ( 'ell pos', ellipsis_position )
+				# print ( 'ell pos', ellipsis_position )
 
 				for idx in range( 0, ellipsis_position ):
-					slicing[ idx ] = obj[ idx ]
+					current = obj[ idx ]
+					if isinstance( current, int ) and current < 0:
+						current = self.shape[ idx ] + current
+					slicing[ idx ] = current
 
 				for idx in range( 1, len( obj ) - ellipsis_position ):
-					print( 'idx', idx)
-					slicing[ len( slicing ) - idx ] = obj[ len( obj ) - idx ]
+					# print( 'idx', idx)
+					current = obj[ len( obj ) - idx ]
+					if isinstance( current, int ) and current < 0:
+						current = self.shape[ len( obj ) - idx ] + current
+					slicing[ len( slicing ) - idx ] = current
 
-				print( "SLICING: ", slicing )
+				# print( "SLICING: ", slicing )
 
 
 			else:
 				raise NotImplementedError( "__getitem__ not yet implemented for advanced indexing! Use int, slice, and Ellipsis only!" )
 
 		elif isinstance( obj, int ):
-			slicing[ 0 ] = shape[0] - obj if obj < 0 else obj
+			slicing[ 0 ] = shape[0] + obj if obj < 0 else obj
+			# print ("SINGLE INT SLICING", slicing)
 
 		else:
 			raise ValueError( "Illegal argument for slicing: {}. Use int, slice, and Ellipsis only!".format( obj ) )
@@ -148,29 +172,42 @@ class ImgLibNumpyArray( np.ndarray ):
 			x if isinstance( x, int ) else slice( x.start if x.start else 0, x.stop if x.stop else rai.dimension( d ), x.step if x.step else 1 ) for d, x in enumerate( slicing[::-1] )
 			]
 
-		# print( obj )
-		# print( reverse_slicing )
-		# print( [ isinstance( x, int ) for x in reverse_slicing ] )
-
 		if ( np.all( [ isinstance( x, int ) for x in reverse_slicing ] ) ):
-			access = rai.randomAccess()
+			# print( "ALL ARE INT", reverse_slicing, obj )
+			access = self.rai.randomAccess()
 			access.setPosition( slicing )
 			val = access.get()
 			return getters[ self.imglib_type ]( val )
 
+		else:
+			hyperslicers = [ (d, pos) for d, pos in enumerate( reverse_slicing ) if isinstance( pos, int ) ]
+			intervalers = [ sl for sl in reverse_slicing if not isinstance( sl, int ) ]
+			result = self.rai
+			for d, pos in hyperslicers[::-1]:
+				result = util.Views.hyperSlice( result, d, pos )
 
-		return None
+			interval_min = [ sl.start for sl in intervalers ] # inclusive
+			interval_dim = [ sl.stop for sl in intervalers ]  # exclusive
+			result = util.Views.offsetInterval( result, interval_min, interval_dim )
 
+			# print( "HS and INTVALS", obj, reverse_slicing, hyperslicers, intervalers, Intervals.minAsLongArray( result ) )
+
+			return ImgLibNumpyArray( result )
+
+	def reshape( self, shape ):
+		print( "RESHAPOING!" )
+		return ImgLibNumpyArray( ReshapeView( self.rai, *shape ) )
 
 
 if __name__ == "__main__":
-	blub = ArrayImgs.floats( 1, 2, 3, 4 )
-	shape = ( 1, 2, 3, 4 )
+	blub = ArrayImgs.floats( 2, 3, 4 )
+	shape = ( 2, 3, 4 )
 	n_elements = int( np.prod( shape ) )
 	data_store = OwningFloatUnsafe( n_elements )
 	dim_array = LongStream.of( *shape ).toArray()
 	print( Arrays.toString( dim_array ) )
 	rai = util.Helpers.toArrayImg( cast( util.Helpers.className( data_store ), data_store ), dim_array )
+	# rai = ArrayImgs.floats( *shape )
 	c = rai.cursor()
 	count = 23
 	while c.hasNext():
@@ -179,19 +216,27 @@ if __name__ == "__main__":
 	# rai.cursor().next().setReal( 3.0 )
 	print( util.Helpers.className( rai.randomAccess().get() ) )
 	print( util.Helpers.classNameSimple( rai.randomAccess().get() ) )
-	arr = ImgLibReferenceGuard( rai )
-	print ( 'sum', np.sum( arr ) )
-	# rai.cursor().next().set( 1.0 )
-	arr[0,0,0,0] = 1
-	arr[ ::2, ... ] = 0
-	print ( 'sum', np.sum( arr ) )
-	c = rai.cursor()
-	while c.hasNext():
-		print (c.next().get())
-	print( "ADDRESS MISMATCH? ", arr.ctypes.data, np.mean( arr ) )
-	print( type( arr ), type( arr.rai ), arr.shape, arr.strides )
-	# arr = ImgLibNumpyArray( rai )
-	# # print( arr.shape, arr.dtype )
-	# b = arr[  0, 1, 2, 3 ]
-	# print( b )
+	# arr = ImgLibReferenceGuard( rai )
+	arr = ImgLibNumpyArray( util.Views.interval( rai, rai ) )
+	# print( "ARR TYPE ", type( arr ), arr.shape, Intervals.dimensionsAsLongArray( arr.rai ) )
+	# print( "ARR SLICED TYPE ", type( arr[0] ) )
+	# print ( arr[0] )
+	# for v0 in range( 1 ):
+	# 	for v2 in range(2):
+	# 		for v1 in range(3):
+	# 			print( "OGE", arr[ v0 ][ v1, v2 ])
+	# print ( 'sum', np.sum( arr ) )
+	# # rai.cursor().next().set( 1.0 )
+	# arr[0,0,0] = 1
+	# arr[ ::1, ... ] = 0
+	# print ( 'sum', np.sum( arr ) )
+	# c = rai.cursor()
+	# while c.hasNext():
+	# 	print (c.next().get())
+	# print( "ADDRESS MISMATCH? ", arr.ctypes.data, np.mean( arr ) )
+	# print( type( arr ), type( arr.rai ), arr.shape, arr.strides )
+	# # arr = ImgLibNumpyArray( rai )
+	# # # print( arr.shape, arr.dtype )
+	# # b = arr[  0, 1, 2, 3 ]
+	# # print( b )
 
