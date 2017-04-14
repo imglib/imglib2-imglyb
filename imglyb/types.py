bd007a3aa8d62219c5724f994bed845e1f1f08da
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-04-13 22:35:23 -0400

    Add IntType and UnsignedIntType

--- imglyb/types.py
+++ imglyb/types.py
@@ -4,4 +4,6 @@ from jnius import autoclass
 NativeType = autoclass( 'net.imglib2.type.NativeType' )
 
 FloatType = autoclass( 'net.imglib2.type.numeric.real.FloatType' )
+IntType = autoclass( 'net.imglib2.type.numeric.integer.IntType' )
+UnsignedIntType = autoclass( 'net.imglib2.type.numeric.integer.UnsignedIntType' )
 VolatileFloatType = autoclass( 'net.imglib2.type.volatiles.VolatileFloatType' )
