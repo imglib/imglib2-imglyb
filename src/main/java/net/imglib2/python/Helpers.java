71009700b0d6122777dde7dd89d37fecb622f2b9
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-05-11 15:42:58 -0400

    Remove obsolete methods

--- src/main/java/net/imglib2/python/Helpers.java
+++ src/main/java/net/imglib2/python/Helpers.java
@@ -1,19 +1,12 @@
 package net.imglib2.python;
 
-import java.awt.Menu;
-
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
 import org.scijava.ui.behaviour.util.Behaviours;
 
-import bdv.img.cache.VolatileCachedCellImg;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.cache.UncheckedCache;
-import net.imglib2.cache.volatiles.UncheckedVolatileCache;
 import net.imglib2.img.array.ArrayImg;
-import net.imglib2.img.basictypelongaccess.unsafe.UnsafeUtil;
 import net.imglib2.img.basictypelongaccess.unsafe.owning.OwningFloatUnsafe;
-import net.imglib2.img.cell.LazyCellImg;
 import net.imglib2.type.Type;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Fraction;
@@ -43,27 +36,12 @@ public class Helpers
 		return o.getClass().getSimpleName().toString();
 	}
 
-	public static long getFirstArrayElementAddress( final Object o )
-	{
-		return UnsafeUtil.getFirstArrayElementAddress( o );
-	}
-
 	public static ArrayImg< FloatType, OwningFloatUnsafe > toArrayImg( final OwningFloatUnsafe store, final long[] dim )
 	{
 		final ArrayImg< FloatType, OwningFloatUnsafe > img = new ArrayImg<>( store, dim, new Fraction() );
 		final FloatType linkedType = new FloatType( img );
 		img.setLinkedType( linkedType );
 		return img;
-	}
-
-	public static < A > LazyCellImg.Get< A > getFromUncheckedCache( final UncheckedCache< Long, A > cache )
-	{
-		return index -> cache.get( index );
-	}
-
-	public static < A > VolatileCachedCellImg.Get< A > getFromUncheckedVolatileCache( final UncheckedVolatileCache< Long, A > cache )
-	{
-		return ( index, hints ) -> cache.get( index, hints );
 	}
 
 	public static < T extends Type< T > > void burnIn( final RandomAccessible< T > source, final RandomAccessibleInterval< T > target )
@@ -86,11 +64,6 @@ public class Helpers
 			if ( s.compareTo( max ) > 0 )
 				max.set( s );
 		return max;
-	}
-
-	public static void main( final String[] args )
-	{
-		final Menu menu = new Menu( "123" );
 	}
 
 }
