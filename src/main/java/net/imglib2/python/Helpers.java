665c3081459770b1e53781ad7b34cd0df541f3d4
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-16 16:22:15 -0400

    Add grid utility methods

--- src/main/java/net/imglib2/python/Helpers.java
+++ src/main/java/net/imglib2/python/Helpers.java
@@ -1,5 +1,7 @@
 package net.imglib2.python;
 
+import java.util.Arrays;
+
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
 import org.scijava.ui.behaviour.util.Behaviours;
 
@@ -7,6 +9,7 @@ import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.basictypelongaccess.unsafe.owning.OwningFloatUnsafe;
+import net.imglib2.img.cell.CellGrid;
 import net.imglib2.type.Type;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Fraction;
@@ -47,23 +50,47 @@ public class Helpers
 	public static < T extends Type< T > > void burnIn( final RandomAccessible< T > source, final RandomAccessibleInterval< T > target )
 	{
 		for ( final Pair< T, T > p : Views.interval( Views.pair( source, target ), target ) )
+		{
 			p.getB().set( p.getA() );
+		}
 	}
 
 	public static < T extends Type< T > & Comparable< T > > T min( final RandomAccessibleInterval< T > source, final T min )
 	{
 		for ( final T s : Views.flatIterable( source ) )
+		{
 			if ( s.compareTo( min ) < 0 )
+			{
 				min.set( s );
+			}
+		}
 		return min;
 	}
 
 	public static < T extends Type< T > & Comparable< T > > T max( final RandomAccessibleInterval< T > source, final T max )
 	{
 		for ( final T s : Views.flatIterable( source ) )
+		{
 			if ( s.compareTo( max ) > 0 )
+			{
 				max.set( s );
+			}
+		}
 		return max;
+	}
+
+	public static CellGrid makeGrid( final long[] dims, final int[] cellSize )
+	{
+		final CellGrid grid = new CellGrid( dims, cellSize );
+		return grid;
+	}
+
+	public static long[] cellMin( final CellGrid grid, final long index )
+	{
+		final long[] min = new long[ grid.numDimensions() ];
+		grid.getCellGridPositionFlat( index, min );
+		Arrays.setAll( min, d -> min[ d ] * grid.cellDimension( d ) );
+		return min;
 	}
 
 }
