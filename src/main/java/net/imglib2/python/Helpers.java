4e7a7001121faa889c58982e4210f34dda62f84e
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-17 16:10:50 -0400

    Add helper methods to replicate VolatileViews.wrapAsVolatile
    For CachedCellImg only!

--- src/main/java/net/imglib2/python/Helpers.java
+++ src/main/java/net/imglib2/python/Helpers.java
@@ -5,11 +5,26 @@ import java.util.Arrays;
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
 import org.scijava.ui.behaviour.util.Behaviours;
 
+import bdv.img.cache.CreateInvalidVolatileCell;
+import bdv.img.cache.VolatileCachedCellImg;
+import bdv.img.cache.VolatileCachedCellImg.Get;
+import bdv.util.volatiles.SharedQueue;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.cache.Cache;
+import net.imglib2.cache.img.CachedCellImg;
+import net.imglib2.cache.ref.WeakRefVolatileCache;
+import net.imglib2.cache.volatiles.CacheHints;
+import net.imglib2.cache.volatiles.CreateInvalid;
+import net.imglib2.cache.volatiles.LoadingStrategy;
+import net.imglib2.cache.volatiles.UncheckedVolatileCache;
+import net.imglib2.cache.volatiles.VolatileCache;
 import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.basictypeaccess.volatiles.VolatileArrayDataAccess;
 import net.imglib2.img.basictypelongaccess.unsafe.owning.OwningFloatUnsafe;
+import net.imglib2.img.cell.Cell;
 import net.imglib2.img.cell.CellGrid;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.Type;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Fraction;
@@ -91,6 +106,43 @@ public class Helpers
 		grid.getCellGridPositionFlat( index, min );
 		Arrays.setAll( min, d -> min[ d ] * grid.cellDimension( d ) );
 		return min;
+	}
+
+	private static class GetFromUncheckedCache< T > implements Get< T >
+	{
+
+		private final UncheckedVolatileCache< Long, T > delegate;
+
+		public GetFromUncheckedCache( final UncheckedVolatileCache< Long, T > delegate )
+		{
+			super();
+			this.delegate = delegate;
+		}
+
+		@Override
+		public T get( final long index, final CacheHints cacheHints )
+		{
+			return delegate.get( index, cacheHints );
+		}
+
+	}
+
+	public static < T extends NativeType< T >, A extends VolatileArrayDataAccess< A > > Object createVolatileCachedCellImg(
+			final CachedCellImg< T, A > img,
+			final boolean dirty )
+	{
+		final CellGrid grid = img.getCellGrid();
+		final T type = img.createLinkedType().createVariable();
+		final Cache< Long, Cell< A > > cache = img.getCache();
+		final SharedQueue queue = new SharedQueue( 1, 1 );
+		final CacheHints hints = new CacheHints( LoadingStrategy.DONTLOAD, 0, false );
+		final CreateInvalid< Long, Cell< A > > createInvalid = CreateInvalidVolatileCell.get( grid, type, dirty );
+		final VolatileCache< Long, Cell< A > > volatileCache = new WeakRefVolatileCache<>( cache, queue, createInvalid );
+		final UncheckedVolatileCache< Long, Cell< A > > unchecked = volatileCache.unchecked();
+		final Get< Cell< A > > get = new GetFromUncheckedCache<>( unchecked );
+		final VolatileCachedCellImg< T, A > volatileImg = new VolatileCachedCellImg<>( grid, type, hints, get );
+		img.randomAccess();
+		return volatileImg;
 	}
 
 }
