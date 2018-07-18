6760e5311667e5bee59be6ad0d9eeafd28e1159e
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-18 12:09:43 -0400

    Add more helpers

--- src/main/java/net/imglib2/python/Helpers.java
+++ src/main/java/net/imglib2/python/Helpers.java
@@ -1,6 +1,9 @@
 package net.imglib2.python;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
 
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
 import org.scijava.ui.behaviour.util.Behaviours;
@@ -13,21 +16,27 @@ import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.cache.Cache;
 import net.imglib2.cache.img.CachedCellImg;
+import net.imglib2.cache.ref.SoftRefLoaderCache;
 import net.imglib2.cache.ref.WeakRefVolatileCache;
 import net.imglib2.cache.volatiles.CacheHints;
 import net.imglib2.cache.volatiles.CreateInvalid;
 import net.imglib2.cache.volatiles.LoadingStrategy;
 import net.imglib2.cache.volatiles.UncheckedVolatileCache;
 import net.imglib2.cache.volatiles.VolatileCache;
+import net.imglib2.img.NativeImg;
 import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
 import net.imglib2.img.basictypeaccess.volatiles.VolatileArrayDataAccess;
+import net.imglib2.img.basictypeaccess.volatiles.array.VolatileByteArray;
 import net.imglib2.img.basictypelongaccess.unsafe.owning.OwningFloatUnsafe;
 import net.imglib2.img.cell.Cell;
 import net.imglib2.img.cell.CellGrid;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.Type;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Fraction;
+import net.imglib2.util.Intervals;
 import net.imglib2.util.Pair;
 import net.imglib2.view.Views;
 
@@ -143,6 +152,70 @@ public class Helpers
 		final VolatileCachedCellImg< T, A > volatileImg = new VolatileCachedCellImg<>( grid, type, hints, get );
 //		img.randomAccess();
 		return volatileImg;
+	}
+
+	public static VolatileByteArray randomByteArray( final int numEntities, final boolean isValid, final Random rng )
+	{
+		final VolatileByteArray vba = new VolatileByteArray( numEntities, isValid );
+		rng.nextBytes( vba.getCurrentStorageArray() );
+		return vba;
+	}
+
+	public static < A > A getFromCache( final Cache< Long, A > cache, final long index ) throws ExecutionException
+	{
+		return cache.get( index );
+	}
+
+	public static < T extends NativeType< T >, A extends ArrayDataAccess< A > > CachedCellImg< T, A > imgFromFunc(
+			final long[] dims,
+			final int[] blockSize,
+			final Function< Long, A > makeAccess,
+			final T t,
+			final A a )
+	{
+		final CellGrid cellGrid = new CellGrid( dims, blockSize );
+		final CacheLoaderFromFunction< Long, Cell< A > > loader = new CacheLoaderFromFunction<>( key -> {
+			final long index = key;
+			final long[] min = new long[ cellGrid.numDimensions() ];
+			final long[] cellGridPosition = new long[ min.length ];
+			final int[] size = new int[ min.length ];
+			cellGrid.getCellGridPositionFlat( index, cellGridPosition );
+			Arrays.setAll( min, d -> cellGrid.getCellMin( d, cellGridPosition[ d ] ) );
+			cellGrid.getCellDimensions( index, min, size );
+			final A access = makeAccess.apply( key );
+			return new Cell<>( size, min, access );
+		} );
+
+		final Cache< Long, Cell< A > > cache = new SoftRefLoaderCache< Long, Cell< A > >().withLoader( loader );
+		final CachedCellImg< T, A > img = new CachedCellImg<>( cellGrid, t.getEntitiesPerPixel(), cache, a );
+		img.setLinkedType( ( T ) t.getNativeTypeFactory().createLinkedType( ( NativeImg ) img ) );
+		return img;
+
+	}
+
+	public static CachedCellImg< UnsignedByteType, VolatileByteArray > randomBytes( final long[] dims, final int[] blockSize )
+	{
+		final CellGrid cellGrid = new CellGrid( dims, blockSize );
+		final CacheLoaderFromFunction< Long, Cell< VolatileByteArray > > loader = new CacheLoaderFromFunction<>( key -> {
+			final long index = key;
+			final long[] min = new long[ cellGrid.numDimensions() ];
+			final long[] cellGridPosition = new long[ min.length ];
+			final int[] size = new int[ min.length ];
+			cellGrid.getCellGridPositionFlat( index, cellGridPosition );
+			Arrays.setAll( min, d -> cellGrid.getCellMin( d, cellGridPosition[ d ] ) );
+			cellGrid.getCellDimensions( index, min, size );
+
+			final VolatileByteArray vba = new VolatileByteArray( ( int ) Intervals.numElements( size ), true );
+			final Random rng = new Random( index );
+			rng.nextBytes( vba.getCurrentStorageArray() );
+			return new Cell<>( size, min, vba );
+		} );
+
+		final Cache< Long, Cell< VolatileByteArray > > cache = new SoftRefLoaderCache< Long, Cell< VolatileByteArray > >().withLoader( loader );
+		final CachedCellImg< UnsignedByteType, VolatileByteArray > img = new CachedCellImg<>( cellGrid, new UnsignedByteType().getEntitiesPerPixel(), cache, new VolatileByteArray( 0, true ) );
+		img.setLinkedType( new UnsignedByteType( img ) );
+		return img;
+
 	}
 
 }
